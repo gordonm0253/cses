6bb3f0b73fadd32ea85ac2d1ed4a4955cda9ed46
gordonm0253 <gordonm0253@gmail.com>
2025-12-27 21:24:56 -0500

    Starting DP section

---
+++ src/dicecombinations.java
@@ -0,0 +1,101 @@
+import java.io.InputStream;
+import java.util.*;
+
+public class dicecombinations {
+    public static void main(String[] args) throws Exception {
+        //Scanner sc = new Scanner(System.in);
+        FastIO sc = new FastIO(System.in);
+        int n = sc.nextInt();
+        long[] ways = new long[n + 1];
+        ways[0] = 1;
+        int mod = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 6; j++) {
+                if (i - j >= 0) {
+                    ways[i] += ways[i - j];
+                }
+            }
+            ways[i] %= mod;
+        }
+        System.out.println(ways[n] % mod);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildGraph(int n, int m, Scanner sc, boolean directed) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            if (!directed) {
+                graph.get(v).add(u);
+            }
+        }
+        return graph;
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildDirectedGraph(int n, int m, Scanner sc) {
+        return buildGraph(n, m, sc, true);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildUndirectedGraph(int n, int m, Scanner sc) {
+        return buildGraph(n, m, sc, false);
+    }
+
+    static final Random random = new Random();
+
+    static void ruffleSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+
+    public static class FastIO {
+        InputStream dis;
+        byte[] buffer = new byte[1 << 17];
+        int pointer = 0;
+
+        public FastIO(InputStream is) {
+            dis = is;
+        }
+
+        int nextInt() throws Exception {
+            int ret = 0;
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+            return (negative) ? -ret : ret;
+        }
+
+        byte nextByte() throws Exception {
+            if (pointer == buffer.length) {
+                dis.read(buffer, 0, buffer.length);
+                pointer = 0;
+            }
+            return buffer[pointer++];
+        }
+
+        public void close() throws Exception {
+            if (dis == null) {
+                return;
+            }
+            dis.close();
+        }
+    }
+}
