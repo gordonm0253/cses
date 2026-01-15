60a3c71ec93ad12a9a796e4f8212ad006b40815c
gordonm0253 <gordonm0253@gmail.com>
2026-01-14 22:35:49 -0500

    Subarray Sums 1

---
+++ src/subarraysums1.java
@@ -0,0 +1,110 @@
+import java.io.InputStream;
+import java.util.*;
+
+public class subarraysums1 {
+    public static void main(String[] args) throws Exception {
+        //Scanner sc = new Scanner(System.in);
+        FastIO sc = new FastIO(System.in);
+        int n = sc.nextInt();
+        long target = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int l = 0;
+        int r = 0;
+        long sum = 0;
+        int good = 0;
+        while (r < n) {
+            sum += a[r];
+            while (sum > target) {
+                sum -= a[l];
+                l++;
+            }
+            if (sum == target) {
+                good++;
+            }
+            r++;
+        }
+        System.out.println(good);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildGraph(int n, int m, FastIO sc, boolean directed) throws Exception {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            if (!directed) {
+                graph.get(v).add(u);
+            }
+        }
+        return graph;
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildDirectedGraph(int n, int m, FastIO sc) throws Exception {
+        return buildGraph(n, m, sc, true);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildUndirectedGraph(int n, int m, FastIO sc) throws Exception {
+        return buildGraph(n, m, sc, false);
+    }
+
+    static final Random random = new Random();
+
+    static void ruffleSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+
+    public static class FastIO {
+        InputStream dis;
+        byte[] buffer = new byte[1 << 17];
+        int pointer = 0;
+
+        public FastIO(InputStream is) {
+            dis = is;
+        }
+
+        int nextInt() throws Exception {
+            int ret = 0;
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+            return (negative) ? -ret : ret;
+        }
+
+        byte nextByte() throws Exception {
+            if (pointer == buffer.length) {
+                dis.read(buffer, 0, buffer.length);
+                pointer = 0;
+            }
+            return buffer[pointer++];
+        }
+
+        public void close() throws Exception {
+            if (dis == null) {
+                return;
+            }
+            dis.close();
+        }
+    }
+}
