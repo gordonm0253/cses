8fcf2f87499d9b5cfe197cd9051065f20911bad1
gordonm0253 <gordonm0253@gmail.com>
2026-05-30 01:06:53 -0400

    Counting towers, removal game

---
+++ src/countingtowers.java
@@ -0,0 +1,111 @@
+import java.io.InputStream;
+import java.util.*;
+
+public class countingtowers {
+    public static void main(String[] args) throws Exception {
+        //Scanner sc = new Scanner(System.in);
+        FastIO sc = new FastIO(System.in);
+        int mod = 1000000007;
+        long[] full = new long[1000001];
+        long[] split = new long[1000001];
+        full[1] = 1;
+        split[1] = 1;
+        for (int i = 2; i <= 1000000; i++) {
+            full[i] += split[i - 1];
+            full[i] += full[i - 1] * 2;
+            split[i] += full[i - 1];
+            split[i] += split[i - 1] * 4;
+            split[i] %= mod;
+            full[i] %= mod;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int t = sc.nextInt(); t > 1; t--) {
+            int n = sc.nextInt();
+            long res = (full[n] + split[n]) % mod;
+            sb.append(res).append('\n');
+        }
+        int n = sc.nextInt();
+        long res = (full[n] + split[n]) % mod;
+        sb.append(res);
+        System.out.println(sb);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildGraph(int n, int m, FastIO sc, boolean directed) throws Exception {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            if (!directed) {
+                graph.get(v).add(u);
+            }
+        }
+        return graph;
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildDirectedGraph(int n, int m, FastIO sc) throws Exception {
+        return buildGraph(n, m, sc, true);
+    }
+
+    public static ArrayList<ArrayList<Integer>> buildUndirectedGraph(int n, int m, FastIO sc) throws Exception {
+        return buildGraph(n, m, sc, false);
+    }
+
+    static final Random random = new Random();
+
+    static void ruffleSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+
+    public static class FastIO {
+        InputStream dis;
+        byte[] buffer = new byte[1 << 17];
+        int pointer = 0;
+
+        public FastIO(InputStream is) {
+            dis = is;
+        }
+
+        int nextInt() throws Exception {
+            int ret = 0;
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+            return (negative) ? -ret : ret;
+        }
+
+        byte nextByte() throws Exception {
+            if (pointer == buffer.length) {
+                dis.read(buffer, 0, buffer.length);
+                pointer = 0;
+            }
+            return buffer[pointer++];
+        }
+
+        public void close() throws Exception {
+            if (dis == null) {
+                return;
+            }
+            dis.close();
+        }
+    }
+}
